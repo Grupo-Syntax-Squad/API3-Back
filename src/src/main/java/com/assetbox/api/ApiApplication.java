7b795ef7f786dc99c6db66a995f41acf50bbc5aa
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-03-25 21:54:55 -0300

    func: Configurações da aplicação e do banco de dados

---
+++ src/src/main/java/com/assetbox/api/ApiApplication.java
@@ -0,0 +1,30 @@
+package com.assetbox.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		Map<String, Object> configuracao = new HashMap<>();
+
+		configuracao.put("server.port", "8080"); // seleção da porta
+
+		configuracao.put("spring.datasource.url", "jdbc:mysql://localhost:3306/assetbox");
+		
+		configuracao.put("spring.datasource.username", "root"); // usuario
+		configuracao.put("spring.datasource.password", "fatec"); // senha
+		
+		configuracao.put("spring.jpa.show-sql", "true"); // mostrar comandos
+		configuracao.put("spring.jpa.hibernate.ddl-auto", "update"); // criar editar
+		
+		SpringApplication app = new SpringApplication(ApiApplication.class);
+		app.setDefaultProperties(configuracao);
+		app.run(args);
+	}
+
+}
