628d0fdd11ca47612579975f5689f15a7b2463cb
Ryan <ryanarajo@yahoo.com.br>
2024-03-28 11:53:43 -0300

    func: Criação da classe Destinatario

--- src/src/main/java/com/assetbox/api/modelos/Destinatario.java
+++ src/src/main/java/com/assetbox/api/modelos/Destinatario.java
@@ -5,16 +5,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
-public class Empresa {
+public class Destinatario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long emp_id;
+	private Long des_id;
 	
 	@Column
-	private String emp_nome;
+	private String des_nome;
 	
 	@Column
-	private Long emp_endereco_id;
+	private String des_email;
+	
+	@Column
+	private String des_senha;
+	
+	@Column
+	private String des_telefone;
+	
+	@Column
+	private Long des_empresa;
+	
+	@OneToOne()
+	@Column
+	private Long des_endereco_id;
 }
