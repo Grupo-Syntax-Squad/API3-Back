115f433f8877f513e519297b9315a379a799d719
mhlreis <81262372+mhlreis@users.noreply.github.com>
2024-04-01 17:09:28 -0300

    func: insercao do controle de Ativo e URL de exibicao de todos ativos cadastrados

---
+++ src/src/main/java/com/assetbox/api/controle/ControleAtivo.java
@@ -0,0 +1,25 @@
+package com.assetbox.api.controle;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.assetbox.api.modelos.Ativo;
+import com.assetbox.api.repositorio.RepositorioAtivo;
+
+@RestController
+
+public class ControleAtivo {
+	
+	@Autowired
+	private RepositorioAtivo ativo;
+	
+	@GetMapping("/ativo")
+	public List<Ativo> obterAtivo(){
+		return ativo.findAll();
+	}
+	
+
+}
