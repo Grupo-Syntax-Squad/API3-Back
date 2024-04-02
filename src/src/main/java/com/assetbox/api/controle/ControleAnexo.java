b14dc61de7d276129f92144e5cd5abc2e939ada6
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-04-02 11:30:15 -0300

    func: Upload de anexo

--- src/src/main/java/com/assetbox/api/controle/ControleAnexo.java
+++ src/src/main/java/com/assetbox/api/controle/ControleAnexo.java
@@ -1,17 +1,36 @@
-package com.assetbox.api.controller; 
+package com.assetbox.api.controle; 
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.assetbox.api.modelos.Anexo;
-
-@Controller
-public class AnexoController {
+@RestController
+public class ControleAnexo {
 
     @PostMapping("/anexo/upload") 
-    public void upload(@RequestParam("file") MultipartFile upload) {
-        Arquivo arquivo = new Arquivo();
-        arquivo.upload("/anexo", upload.getOriginalFilename(), upload.getInputStream());
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile upload) {
+        String caminho = "../../Pictures/AssetBox/" + UUID.randomUUID() + "." + extrairExtensao(upload.getOriginalFilename());
+        try {
+            Files.copy(upload.getInputStream(), Path.of(caminho), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println(Path.of(caminho));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            String mensagemErro = e.getMessage();
+            System.out.println(mensagemErro);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private String extrairExtensao(String nomeArquivo) {
+        int i = nomeArquivo.lastIndexOf(".");
+        return nomeArquivo.substring(i + 1);
     }
 }
--- src/src/main/java/com/assetbox/api/controle/ControleAtivo.java
+++ src/src/main/java/com/assetbox/api/controle/ControleAtivo.java
@@ -9,14 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.assetbox.api.modelos.Ativo;
 import com.assetbox.api.repositorio.RepositorioAtivo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @RestController
-
 public class ControleAtivo {
 	
 	@Autowired
