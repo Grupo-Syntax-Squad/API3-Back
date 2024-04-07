3f153eea5199149959894391cab117e67f717d05
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-04-07 11:09:25 -0300

    func: Criação da classe CadastroAtivo

--- src/src/main/java/com/assetbox/api/controle/ControleAtivo.java
+++ src/src/main/java/com/assetbox/api/controle/ControleAtivo.java
@@ -3,12 +3,14 @@ package com.assetbox.api.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.assetbox.api.modelos.Ativo;
 import com.assetbox.api.repositorio.RepositorioAtivo;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
@@ -20,5 +22,10 @@ public class ControleAtivo {
 	@GetMapping("/ativo")
 	public List<Ativo> obterAtivo(){
 		return ativo.findAll();
+	}
+
+	@PostMapping("/cadastrar/ativo")
+	public Ativo cadastrarAtivo(@RequestBody Ativo ativo) {
+		return this.ativo.save(ativo);
 	}
 }
--- src/src/main/java/com/assetbox/api/modelos/Ativo.java
+++ src/src/main/java/com/assetbox/api/modelos/Ativo.java
@@ -6,11 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
-import com.assetbox.api.modelos.Administrador;
-import com.assetbox.api.modelos.Destinatario;
 
 @Entity
 public class Ativo {
