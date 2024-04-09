babce7a49660abb874d4ee28ed20df1f9a428c67
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-04-09 11:58:16 -0300

    func: Upload de imagem

--- src/main/java/com/assetbox/api/controle/ControleImagem.java
+++ src/main/java/com/assetbox/api/controle/ControleImagem.java
@@ -1,22 +1,35 @@
 package com.assetbox.api.controle;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.assetbox.api.modelos.Ativo;
+import com.assetbox.api.modelos.Imagem;
+import com.assetbox.api.repositorio.RepositorioImagem;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
 public class ControleImagem {
+    @Autowired
+    private RepositorioImagem repositorioImagem;
+
     @GetMapping("/imagem")
     public ResponseEntity<InputStreamResource> getImagem(@RequestBody String caminhoImagem) {
         Path caminhoAbsoluto = Paths.get(caminhoImagem);
@@ -31,5 +44,28 @@ public class ControleImagem {
             .contentType(MediaType.parseMediaType("image/*"))
             .body(new InputStreamResource(inputStream));
     }
-    
+
+    @PostMapping("/imagem")
+    public ResponseEntity<String> postImagem(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                String pathString = "./" + file.getOriginalFilename();
+                Path path = Paths.get("./" + file.getOriginalFilename());
+                Files.write(path, bytes);
+                
+                Imagem imagem = new Imagem();
+                imagem.setIma_caminho("./" + file.getOriginalFilename());
+                imagem.setIma_nome(file.getOriginalFilename());
+                repositorioImagem.save(imagem);
+
+                return ResponseEntity.ok().body("Imagem enviada com sucesso!");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar a imagem.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Selecione uma imagem para fazer o upload.");
+        }
+    }
 }
--- src/main/java/com/assetbox/api/modelos/Imagem.java
+++ src/main/java/com/assetbox/api/modelos/Imagem.java
@@ -13,12 +13,32 @@ public class Imagem {
 	private Long ima_id;
 	
 	@Column
-	private String ima_ativo_id;
-	
-	@Column
 	private String ima_nome;
 	
 	@Column
 	private String ima_caminho;
-	
+
+	public Long getIma_id() {
+		return ima_id;
+	}
+
+	public void setIma_id(Long ima_id) {
+		this.ima_id = ima_id;
+	}
+
+	public String getIma_nome() {
+		return ima_nome;
+	}
+
+	public void setIma_nome(String ima_nome) {
+		this.ima_nome = ima_nome;
+	}
+
+	public String getIma_caminho() {
+		return ima_caminho;
+	}
+
+	public void setIma_caminho(String ima_caminho) {
+		this.ima_caminho = ima_caminho;
+	}
 }
