711f9a2b89da150471774f8b0bd5c7d2611740f7
Ryan <ryanarajo@yahoo.com.br>
2024-04-10 08:52:10 -0300

    func: buscar ativo por id

--- src/main/java/com/assetbox/api/controle/ControleAtivo.java
+++ src/main/java/com/assetbox/api/controle/ControleAtivo.java
@@ -1,41 +1,44 @@
+
 package com.assetbox.api.controle;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.assetbox.api.modelos.Anexo;
 import com.assetbox.api.modelos.Ativo;
-import com.assetbox.api.repositorio.RepositorioAnexo;
 import com.assetbox.api.repositorio.RepositorioAtivo;
-import com.assetbox.api.repositorio.RepositorioDestinatario;
-import com.assetbox.api.repositorio.RepositorioImagem;
 
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class ControleAtivo {
 	@Autowired
-	private RepositorioAnexo anexoRepositorio;
-
-	@Autowired
 	private RepositorioAtivo ativoRepositorio;
 
-	@Autowired
-	private RepositorioDestinatario destinatarioRepositorio;
-
-	@Autowired
-	private RepositorioImagem imagemRepositorio;
-
 	@GetMapping("/ativos")
-	public List<Ativo> obterAtivo() {
+	public List<Ativo> obterAtivos() {
 		return ativoRepositorio.findAll();
 	}
 
 	@PostMapping("/cadastrar/ativo")
 	public void cadastrarAtivo(@RequestBody Ativo ativo) {
+	}
+	
+	@GetMapping("/ativo/{id}")
+	public ResponseEntity<Ativo> obterAtivo(@PathVariable long id) {
+		Optional<Ativo> ativo = ativoRepositorio.findById(id);
+		if (ativo == null) {
+			ResponseEntity<Ativo> resposta = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			return resposta;
+		}  else {
+			ResponseEntity<Ativo> resposta = new ResponseEntity<Ativo>(HttpStatus.FOUND);
+			return resposta;
+		}
 	}
 }
