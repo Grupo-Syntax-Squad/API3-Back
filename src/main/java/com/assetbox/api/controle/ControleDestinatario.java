c0e187683207b3eb6289d986bfaf25b7b4699cb5
Ryan <ryanarajo@yahoo.com.br>
2024-04-12 10:34:58 -0300

    refatorar: remoção de import não utilizado

--- src/main/java/com/assetbox/api/controle/ControleDestinatario.java
+++ src/main/java/com/assetbox/api/controle/ControleDestinatario.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.assetbox.api.modelos.Ativo;
 import com.assetbox.api.modelos.Destinatario;
 import com.assetbox.api.repositorio.RepositorioDestinatario;
 
