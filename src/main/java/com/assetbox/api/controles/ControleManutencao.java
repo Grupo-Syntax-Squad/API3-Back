bb2b8dffa0651d9837ffcc99ed32008054974868
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-04-18 15:58:37 -0300

    func: Tratamento de erros nas rotas GET e POST de manutenção

--- src/main/java/com/assetbox/api/controles/ControleManutencao.java
+++ src/main/java/com/assetbox/api/controles/ControleManutencao.java
@@ -10,10 +10,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -34,11 +32,11 @@ public class ControleManutencao {
     }
 
     @PostMapping("/manutencoes")
-    public ResponseEntity<Manutencao> postManutencao(@RequestBody Manutencao manutencao) {
+    public ResponseEntity<?> postManutencao(@RequestBody Manutencao manutencao) {
         try {
-            return new ResponseEntity<>(repositorioManutencao.save(manutencao), HttpStatus.OK);
+            return new ResponseEntity<Manutencao>(repositorioManutencao.save(manutencao), HttpStatus.OK);
         } catch(Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/com/assetbox/api/modelos/Manutencao.java
+++ src/main/java/com/assetbox/api/modelos/Manutencao.java
@@ -1,9 +1,9 @@
 package com.assetbox.api.modelos;
 
-import java.sql.Date;
-import java.sql.Time;
 
-import org.hibernate.annotations.Collate;
+import java.sql.Time;
+import java.util.Date;
+
 
 import com.assetbox.api.enums.ManutencaoStatus;
 
@@ -21,19 +21,19 @@ import jakarta.persistence.ManyToOne;
 public class Manutencao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long man_id;
 
-    // @ManyToOne
-    // @JoinColumn(name = "man_ativo_id")
-    // private Ativo man_ativo_id;
+    @ManyToOne
+    @JoinColumn(name = "man_ativo_id")
+    private Ativo man_ativo_id;
 
     // @ManyToOne
     // @JoinColumn(name = "ma_administrador_id")
     // private Administrador man_administrador_id;
 
-    // @ManyToOne
-    // @JoinColumn(name = "man_endereco_id")
-    // private Endereco man_endereco_id;
+    @ManyToOne
+    @JoinColumn(name = "man_endereco_id")
+    private Endereco man_endereco_id;
 
     @Column(nullable = false)
     private String man_desc;
@@ -41,12 +41,76 @@ public class Manutencao {
     @Column(nullable = false)
     private Date man_data;
 
-    // @Column(nullable = false)
-    // private Time man_hora;
+    @Column(nullable = false)
+    private Time man_hora;
 
     @Column(nullable = false)
     private String man_localizacao;
 
     @Enumerated(EnumType.STRING)
     private ManutencaoStatus man_status;
+
+    public Long getMan_id() {
+        return man_id;
+    }
+
+    public void setMan_id(Long man_id) {
+        this.man_id = man_id;
+    }
+
+    public Ativo getMan_ativo_id() {
+        return man_ativo_id;
+    }
+
+    public void setMan_ativo_id(Ativo man_ativo_id) {
+        this.man_ativo_id = man_ativo_id;
+    }
+
+    public Endereco getMan_endereco_id() {
+        return man_endereco_id;
+    }
+
+    public void setMan_endereco_id(Endereco man_endereco_id) {
+        this.man_endereco_id = man_endereco_id;
+    }
+
+    public String getMan_desc() {
+        return man_desc;
+    }
+
+    public void setMan_desc(String man_desc) {
+        this.man_desc = man_desc;
+    }
+
+    public Date getMan_data() {
+        return man_data;
+    }
+
+    public void setMan_data(Date man_data) {
+        this.man_data = man_data;
+    }
+
+    public Time getMan_hora() {
+        return man_hora;
+    }
+
+    public void setMan_hora(Time man_hora) {
+        this.man_hora = man_hora;
+    }
+
+    public String getMan_localizacao() {
+        return man_localizacao;
+    }
+
+    public void setMan_localizacao(String man_localizacao) {
+        this.man_localizacao = man_localizacao;
+    }
+
+    public ManutencaoStatus getMan_status() {
+        return man_status;
+    }
+
+    public void setMan_status(ManutencaoStatus man_status) {
+        this.man_status = man_status;
+    }
 }
