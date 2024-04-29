4272a1a3552ddecd61fe85641315c684500fcbf1
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-04-29 20:25:51 -0300

    func: Inserindo um admnistrador no banco ao inicializar a aplicação

--- src/main/java/com/assetbox/api/ApiApplication.java
+++ src/main/java/com/assetbox/api/ApiApplication.java
@@ -1,13 +1,28 @@
 package com.assetbox.api;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ApiApplication {
+import com.assetbox.api.modelos.Administrador;
+import com.assetbox.api.repositorios.RepositorioAdministrador;
 
+@SpringBootApplication
+public class ApiApplication implements CommandLineRunner {
+	@Autowired
+	RepositorioAdministrador repositorioAdministrador;
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		if (repositorioAdministrador.count() == 0) {
+			Administrador administrador = new Administrador("admin", "admin@gmail.com", "123", "12999999999");
+			repositorioAdministrador.save(administrador);
+			System.out.println("Administrador cadastrado com sucesso!");
+		}
+	}
 }
