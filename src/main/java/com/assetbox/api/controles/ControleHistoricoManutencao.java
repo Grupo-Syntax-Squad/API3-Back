6a3971b52220c15ced4059f513bd50be30f19424
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-05-01 10:54:09 -0300

    func: Rota get para puxar todos os históricos de manutenção

---
+++ src/main/java/com/assetbox/api/controles/ControleHistoricoManutencao.java
@@ -0,0 +1,36 @@
+package com.assetbox.api.controles;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.assetbox.api.modelos.HistoricoManutencao;
+import com.assetbox.api.repositorios.RepositorioHistoricoManutencao;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("historicoManutencoes")
+public class ControleHistoricoManutencao {
+    @Autowired
+    private RepositorioHistoricoManutencao repositorioHistoricoManutencao;
+
+    @GetMapping("")
+    public ResponseEntity<List<HistoricoManutencao>> getHistoricoManutencoes() {
+        try {
+            return ResponseEntity.ok(repositorioHistoricoManutencao.findAll());
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    
+}
