4e4c997ebe5d3f2f7e03e0d2a1335fec1482958c
Ryan <ryanarajo@yahoo.com.br>
2024-05-23 20:41:01 -0300

    refatorar: adição e adequação de verbos HTTP

--- src/main/java/com/assetbox/api/controles/ControleImagem.java
+++ src/main/java/com/assetbox/api/controles/ControleImagem.java
@@ -1,6 +1,5 @@
 package com.assetbox.api.controles;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,11 +22,12 @@ import com.assetbox.api.modelos.Imagem;
 import com.assetbox.api.repositorios.RepositorioImagem;
 
 @RestController
+@RequestMapping("imagens")
 public class ControleImagem {
     @Autowired
     private RepositorioImagem repositorioImagem;
 
-    @GetMapping("/imagens/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Resource> getImagem(@PathVariable Long id) {
         try {
             Imagem imagem = repositorioImagem.findById(id).get();
@@ -45,7 +46,7 @@ public class ControleImagem {
         }
     }
 
-    @PostMapping("/imagens")
+    @PostMapping
     public ResponseEntity<?> uploadImagem(@RequestParam("file") MultipartFile file) {
         Path path = Paths.get("src/main/resources/static/" + file.getOriginalFilename());
         
@@ -62,9 +63,9 @@ public class ControleImagem {
             repositorioImagem.save(imagem);
             
             return ResponseEntity.ok().body(imagem);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/assetbox/api/controles/ControleTipo.java
+++ src/main/java/com/assetbox/api/controles/ControleTipo.java
@@ -5,34 +5,48 @@ import org.springframework.web.bind.annotation.RestController;
 import com.assetbox.api.modelos.Tipo;
 import com.assetbox.api.repositorios.RepositorioTipo;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 
 @RestController
+@RequestMapping("tipos")
 public class ControleTipo {
     @Autowired
     private RepositorioTipo repositorioTipo;
 
-    @GetMapping("/tipos")
-    public ResponseEntity<List<Tipo>> getTipos() {
-        return ResponseEntity.ok().body(repositorioTipo.findAll());
+    @GetMapping
+    public ResponseEntity<?> getTipos() {
+        try{
+            return ResponseEntity.ok().body(repositorioTipo.findAll());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping("/tipos/{id}")
-    public ResponseEntity<Tipo> getTipo(@PathVariable Long id) {
-        return ResponseEntity.ok().body(repositorioTipo.findById(id).get());
+    @GetMapping("{id}")
+    public ResponseEntity<?> getTipo(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok().body(repositorioTipo.findById(id).get());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
     }
 
-    @PostMapping("/tipos")
-    public ResponseEntity<Tipo> postTipo(@RequestBody Tipo tipo) {
-        return ResponseEntity.ok().body(repositorioTipo.save(tipo));
+    @PostMapping
+    public ResponseEntity<?> postTipo(@RequestBody Tipo tipo) {
+        try {
+            return ResponseEntity.ok().body(repositorioTipo.save(tipo));
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
